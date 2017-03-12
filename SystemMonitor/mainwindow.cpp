902d1e7345f4e220e158a21e3fd774edb780c79b
Andrea Perez Quintana <alu0100843101@ull.edu.es>
2017-03-12 12:51:42 +0100

    Terminado código Procesos

--- SystemMonitor/mainwindow.cpp
+++ SystemMonitor/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+/********************** PUBLIC: **********************/
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -14,13 +16,81 @@ MainWindow::MainWindow(QWidget *parent) :
 
     connect(mythread_, SIGNAL(dataSend()),this, SLOT(dataReceive()));
 
-    ui->tableWidget_4->setColumnCount(2);
+    ui->tableWidget_4->setColumnCount(2);       //Sensores.
+    ui->tableWidget_4->setHorizontalHeaderLabels(QString("NOMBRE; INFO").split(";"));
+    ui->tableWidget->setColumnCount(5);         //Procesos.
+    ui->tableWidget->setHorizontalHeaderLabels(QString("NOMBRE; PID; CMDLINE; HILOS; PROPIETARIO").split(";"));
+
+    //Qt Documentation: http://doc.qt.io/qt-5/qtimer.html#details
+    time_ = new QTimer(this);
+    connect(time_,SIGNAL(timeout()), this,
+            SLOT(myTimeout()));                 //Se conectan las señales de timeout.
+    time_->start(5000);                         //milisegundos -> 5000 mS = 5 segundos.
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+Process MainWindow::get_proc(QDir p)
+{
+    Process proc_;
+    QFile file_;
+    QByteArray contain_;
+
+    QFileInfoList fileInfoList_ = p.entryInfoList(QDir::AllEntries | QDir::Hidden);
+
+    for(auto files_: fileInfoList_){
+           file_.setFileName(files_.absoluteFilePath());
+           file_.open(QIODevice::ReadOnly | QIODevice::Text);
+
+           if(files_.fileName()=="cmdline"){
+              contain_=file_.readLine();
+              //Qt Documentation: http://doc.qt.io/qt-5/qstring.html#trimmed
+              proc_.cmdline=contain_.trimmed();  //Espacios.
+           }
+
+           else if (files_.fileName()=="status")
+           {
+               while(file_.atEnd()){
+                  while(file_.readLine(6)!="Name:"){
+                   file_.readLine();
+                  }
+                  contain_=file_.readLine();
+                  proc_.name=contain_.trimmed();
+
+                  while(file_.readLine(5)!="Pid:"){
+                   file_.readLine();
+                  }
+                  contain_=file_.readLine();
+                  proc_.pid=contain_.trimmed();
+
+                  while(file_.readLine(5)!="Uid:"){
+                    file_.readLine();
+                  }
+                  contain_=file_.readLine(6);
+                  //proc_.owner=contain_.trimmed();
+
+                  proc_.owner=contain_.toInt();
+                  file_.readLine();
+
+
+                  while(file_.readLine(9)!="Threads:"){
+                   file_.readLine();
+                  }
+                      contain_=file_.readLine();
+                      proc_.threads=contain_.trimmed();
+
+                }
+           }
+
+           file_.close();
+     }
+     return proc_;
+}
+
+/********************** PRIVATE SLOTS: **********************/
 
 void MainWindow::dataReceive()
 {
@@ -30,42 +100,132 @@ void MainWindow::dataReceive()
     if(textSensor_!=""){
         if(pair.second!=textSensor_)
         {
-            if(ui->tableWidget_4->item(line_,0)==NULL)
+            if(ui->tableWidget_4->item(lineSensor_,0)==NULL)
             {
-                ui->tableWidget_4->insertRow(line_);
+                ui->tableWidget_4->insertRow(lineSensor_);
 
-                ui->tableWidget_4->setItem(line_,0,new QTableWidgetItem(pair.first));
-                ui->tableWidget_4->setItem(line_,1,new QTableWidgetItem(pair.second));
+                ui->tableWidget_4->setItem(lineSensor_,0,new QTableWidgetItem(pair.first));
+                ui->tableWidget_4->setItem(lineSensor_,1,new QTableWidgetItem(pair.second));
 
-                line_++;
+                lineSensor_++;
 
             } else
             {
-                ui->tableWidget_4->setItem(line_,0,new QTableWidgetItem(pair.first));
-                ui->tableWidget_4->setItem(line_,1,new QTableWidgetItem(pair.second));
+                ui->tableWidget_4->setItem(lineSensor_,0,new QTableWidgetItem(pair.first));
+                ui->tableWidget_4->setItem(lineSensor_,1,new QTableWidgetItem(pair.second));
             }
 
-            line_++;
+            lineSensor_++;
         } else
         {
-            line_ = 0;
+            lineSensor_ = 0;
 
-            ui->tableWidget_4->setItem(line_,0,new QTableWidgetItem(pair.first));
-            ui->tableWidget_4->setItem(line_,1,new QTableWidgetItem(pair.second));
+            ui->tableWidget_4->setItem(lineSensor_,0,new QTableWidgetItem(pair.first));
+            ui->tableWidget_4->setItem(lineSensor_,1,new QTableWidgetItem(pair.second));
 
-            line_++;
+            lineSensor_++;
         }
 
     } else
     {
-        ui->tableWidget_4->insertRow(line_);
+        ui->tableWidget_4->insertRow(lineSensor_);
 
-        ui->tableWidget_4->setItem(line_,0,new QTableWidgetItem(pair.first));
-        ui->tableWidget_4->setItem(line_,1,new QTableWidgetItem(pair.second));
+        ui->tableWidget_4->setItem(lineSensor_,0,new QTableWidgetItem(pair.first));
+        ui->tableWidget_4->setItem(lineSensor_,1,new QTableWidgetItem(pair.second));
 
         entradaSensor_ = ui->tableWidget_4->item(0,1);
         textSensor_ = entradaSensor_->text();
 
-        line_++;
+        lineSensor_++;
     }
+}
+
+void MainWindow::myTimeout()
+{
+    QDir dir_;
+    dir_.setPath("/proc");
+    dir_.setNameFilters(QStringList()<<"1*"<<"2*"<<"3*"<<"4*"<<"5*"<<"6*"<<"7*"<<"8*"<<"9*"<<"status"<<"cmdline");
+    QStringList fileList_ = dir_.entryList();
+
+    for(auto entry: fileList_)
+    {
+        //Qt Documentation: doc.qt.io/qt-5/qfuturewatcher.html#QFutureWatcher
+        QFutureWatcher <Process> *watcher_ = new QFutureWatcher<Process>;
+        dir_.cd(entry);
+        QFuture<Process> runProc_ = QtConcurrent::run(this,&MainWindow::get_proc,dir_);
+        watcher_->setFuture(runProc_);
+
+        //connect(&watcher_, SIGNAL(finished()), &QFutureWatcher<Process>, SLOT(handleFinished));
+        connect(watcher_, &QFutureWatcher<Process>::finished,[=]()
+        {
+            //Linux Documentation: http://man7.org/linux/man-pages/man3/getpwnam.3.html
+            //Qt Documetation: http://doc.qt.io/qt-5/qfuturewatcher.html#result
+            psswd=getpwuid(runProc_.result().owner);
+
+            if(psswd!=NULL)
+            {
+                qDebug() << "Ok";
+            }
+            else psswd=getpwuid(1000);
+
+            if(textProcesos_!="")
+            {
+                 if(runProc_.result().pid!=textProcesos_)
+                 {
+                     if(ui->tableWidget->item(lineProcesos_,0)==NULL)
+                     {
+                         ui->tableWidget->insertRow(lineProcesos_);
+
+                         ui->tableWidget->setItem(lineProcesos_,0,new QTableWidgetItem(runProc_.result().name));
+                         ui->tableWidget->setItem(lineProcesos_,1,new QTableWidgetItem(runProc_.result().pid));
+                         ui->tableWidget->setItem(lineProcesos_,2,new QTableWidgetItem(runProc_.result().cmdline));
+                         ui->tableWidget->setItem(lineProcesos_,3,new QTableWidgetItem(runProc_.result().threads));
+                         ui->tableWidget->setItem(lineProcesos_,4,new QTableWidgetItem(psswd->pw_name));
+
+                         lineProcesos_++;
+                     } else
+                     {
+                         ui->tableWidget->setItem(lineProcesos_,0,new QTableWidgetItem(runProc_.result().name));
+                         ui->tableWidget->setItem(lineProcesos_,1,new QTableWidgetItem(runProc_.result().pid));
+                         ui->tableWidget->setItem(lineProcesos_,2,new QTableWidgetItem(runProc_.result().cmdline));
+                         ui->tableWidget->setItem(lineProcesos_,3,new QTableWidgetItem(runProc_.result().threads));
+                         ui->tableWidget->setItem(lineProcesos_,4,new QTableWidgetItem(psswd->pw_name));
+
+                         lineProcesos_++;
+                     }
+                 } else
+                 {
+                     lineProcesos_ = 0;
+
+                     ui->tableWidget->setItem(lineProcesos_,0,new QTableWidgetItem(runProc_.result().name));
+                     ui->tableWidget->setItem(lineProcesos_,1,new QTableWidgetItem(runProc_.result().pid));
+                     ui->tableWidget->setItem(lineProcesos_,2,new QTableWidgetItem(runProc_.result().cmdline));
+                     ui->tableWidget->setItem(lineProcesos_,3,new QTableWidgetItem(runProc_.result().threads));
+                     ui->tableWidget->setItem(lineProcesos_,4,new QTableWidgetItem(psswd->pw_name));
+
+                     lineProcesos_++;
+                 }
+            } else
+            {
+                 ui->tableWidget->insertRow(lineProcesos_);
+
+                 ui->tableWidget->setItem(lineProcesos_,0,new QTableWidgetItem(runProc_.result().name));
+                 ui->tableWidget->setItem(lineProcesos_,1,new QTableWidgetItem(runProc_.result().pid));
+                 ui->tableWidget->setItem(lineProcesos_,2,new QTableWidgetItem(runProc_.result().cmdline));
+                 ui->tableWidget->setItem(lineProcesos_,3,new QTableWidgetItem(runProc_.result().threads));
+                 ui->tableWidget->setItem(lineProcesos_,4,new QTableWidgetItem(psswd->pw_name));
+
+                 lineProcesos_++;
+
+                 entradaProcesos_ = ui->tableWidget->item(0,1);
+                 textProcesos_ = entradaProcesos_->text();
+             }
+
+             watcher_->waitForFinished();
+             watcher_->deleteLater();
+         });
+
+        dir_.cd("..");
+    }
+
 }
--- SystemMonitor/mainwindow.h
+++ SystemMonitor/mainwindow.h
@@ -8,14 +8,31 @@
 #include <sys/types.h>      //Información de una cuenta de usuario a través de su nombre o id.
 #include <pwd.h>            //Información de una cuenta de usuario a través de su nombre o id.
 #include <QVector>          //Almacenar proceso en una estructura cola o vector.
+#include <QTimer>
+#include <QtConcurrent/QtConcurrent>
+                            //Cada tarea se asigna al agrupamiento usando QtConcurrent.
+#include <QFuture>          //QtConcurrent devuelve tipo QFuture.
+#include <QTimer>           //Emite señales de temporizador cada intervalo de tiempo.
 
 #include "mythread.h"
+
+struct Process
+{
+    QString pid;
+    QString cmdline;
+    QString name;
+    QString threads;
+    //QString owner;
+    int owner;
+};
 
 namespace Ui {
 class MainWindow;
 }
 
 class MyThread;
+//Qt Documentation: http://doc.qt.io/qt-5/qtimer.html#details
+class QTimer;
 
 class MainWindow : public QMainWindow
 {
@@ -25,8 +42,12 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    Process get_proc(QDir p);
+
 private slots:
     void dataReceive();
+    void myTimeout();               //Señal timeout() [clase QTimer].
+    //void handleFinished();
 
 private:
     Ui::MainWindow *ui;
@@ -35,8 +56,17 @@ private:
 
     QTableWidgetItem *entradaSensor_;
     QString textSensor_;
-    int line_ = 0;
+    int lineSensor_ = 0;
 
+    QVector <Process> procesos_;
+
+    QTableWidgetItem *entradaProcesos_;
+    QString textProcesos_;
+    int lineProcesos_ = 0;
+
+    QTimer *time_;
+
+    passwd *psswd;
 };
 
 #endif // MAINWINDOW_H
